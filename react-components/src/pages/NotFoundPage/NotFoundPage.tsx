a001bddb918cd1b6d1e4043b748228f49f351cbc
Kavume <e.gorbacheva404@gmail.com>
2023-03-14 15:43:11 +0100

    feat: add 404 page, create button component

--- react-components/src/pages/NotFoundPage/NotFoundPage.tsx
+++ react-components/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import styles from './NotFoundPage.module.scss';
+import Button from "../../components/common/Button";
+import {Link} from "react-router-dom";
 
 const NotFoundPage = () => {
     return (
-        <div>
-            NotFoundPage
+        <div className={styles.main}>
+            <div className={styles.textWrap}>
+                <h2 className={styles.title}>Something's wrong here!</h2>
+                <p className={styles.text}>This is a 404 error, which means you have clicked on a wrong link or entered
+                    an invalid URL</p>
+            </div>
+            <Link to='/'>
+                <Button text={'Go back'}/>
+            </Link>
         </div>
     );
 };
