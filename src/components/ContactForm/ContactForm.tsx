809ded641d4a722472add333cd89beea8cb9171d
Kavume <e.gorbacheva404@gmail.com>
2023-03-22 20:45:58 +0100

    feat: add checkbox input component

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -3,12 +3,21 @@ import styles from './ContactForm.module.scss';
 import { TextInput } from '../common/TextInput';
 import { DateInput } from '../common/DateInput';
 import { DropdownInput } from '../common/DropdownInput';
+import { CheckboxInput } from '../common/CheckboxInput';
 
 const dropdownInputData = [
   { value: 'Male' },
   { value: 'Female' },
   { value: 'Other' },
   { value: 'Prefer not to say' },
+];
+
+const checkboxInputConsentData = [{ value: 'I consent to my personal data' }];
+const checkboxInputContactData = [
+  { value: 'SMS' },
+  { value: 'Call' },
+  { value: 'Email' },
+  { value: 'Video chat' },
 ];
 
 class ContactForm extends Component {
@@ -20,11 +29,21 @@ class ContactForm extends Component {
           <TextInput label={'Name'} placeholder={'Enter your name'} />
           <TextInput label={'Surname'} placeholder={'Enter your surname'} />
           <DropdownInput
-            label={'Gender:'}
+            label={'Gender'}
             placeholder={'Please select'}
             options={dropdownInputData}
           />
           <DateInput label={'Birth date'} />
+          <CheckboxInput
+            title={'Agreement'}
+            checkboxes={checkboxInputConsentData}
+            name={'agreement'}
+          />
+          <CheckboxInput
+            title={'Choose how we can contact'}
+            checkboxes={checkboxInputContactData}
+            name={'contacts'}
+          />
         </div>
       </div>
     );
---
+++ src/components/common/CheckboxInput/CheckboxInput.tsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import inputStyles from './../styles/Input.module.scss';
+import styles from './CheckboxInput.module.scss';
+
+interface CheckboxInputProps {
+  checkboxes: { value: string }[];
+  title: string;
+  name: string;
+}
+
+class CheckboxInput extends Component<CheckboxInputProps> {
+  constructor(props: CheckboxInputProps) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className={inputStyles.inputWrapper}>
+        <p className={inputStyles.label}>{this.props.title}</p>
+        {this.props.checkboxes.map((item) => (
+          <div className={styles.checkboxWrapper} key={item.value}>
+            <input
+              className={styles.checkbox}
+              type="checkbox"
+              id={item.value}
+              name={this.props.name}
+            />
+            <label htmlFor={item.value}>{item.value}</label>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default CheckboxInput;
