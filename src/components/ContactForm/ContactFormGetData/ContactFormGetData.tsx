61d33be71988d73052f33d14ec84b57e0e2a85a7
Kavume <e.gorbacheva404@gmail.com>
2023-03-28 13:58:07 +0200

    refactor: convert contact form get data component to functional

--- src/components/ContactForm/ContactFormGetData/ContactFormGetData.tsx
+++ src/components/ContactForm/ContactFormGetData/ContactFormGetData.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styles from './ContactFormGetData.module.scss';
 
 interface ContactFormGetDataProps {
@@ -14,28 +14,22 @@ interface ContactFormGetDataProps {
   };
 }
 
-class ContactFormGetData extends Component<ContactFormGetDataProps> {
-  constructor(props: ContactFormGetDataProps) {
-    super(props);
-  }
-
-  render() {
-    const image = this.props.formData.image?.split('\\').pop();
-    return (
-      <div className={styles.listWrapper}>
-        <ol className={styles.list}>
-          <li>Name: {this.props.formData.name}</li>
-          <li>Surname: {this.props.formData.surname}</li>
-          <li>Gender: {this.props.formData.gender}</li>
-          <li>Birth Date: {this.props.formData.birthDate}</li>
-          <li>Agreement: {this.props.formData.agreement}</li>
-          <li>Contacts: {this.props.formData.contacts?.join(',')}</li>
-          <li>Rate: {this.props.formData.rate}</li>
-          <li>Image: {image}</li>
-        </ol>
-      </div>
-    );
-  }
-}
+const ContactFormGetData = (props: ContactFormGetDataProps) => {
+  const image = props.formData.image?.split('\\').pop();
+  return (
+    <div className={styles.listWrapper}>
+      <ol className={styles.list}>
+        <li>Name: {props.formData.name}</li>
+        <li>Surname: {props.formData.surname}</li>
+        <li>Gender: {props.formData.gender}</li>
+        <li>Birth Date: {props.formData.birthDate}</li>
+        <li>Agreement: {props.formData.agreement}</li>
+        <li>Contacts: {props.formData.contacts?.join(',')}</li>
+        <li>Rate: {props.formData.rate}</li>
+        <li>Image: {image}</li>
+      </ol>
+    </div>
+  );
+};
 
 export default ContactFormGetData;
--- src/components/SearchBar/SearchIcon/SearchIcon.tsx
+++ src/components/SearchBar/SearchIcon/SearchIcon.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 interface SearchIconProps {
   fill: string;
@@ -6,38 +6,26 @@ interface SearchIconProps {
   className: string;
 }
 
-class SearchIcon extends Component<SearchIconProps> {
-  constructor(props: SearchIconProps) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <svg
-        className={this.props.className}
-        width="21"
-        height="20"
-        viewBox="0 0 21 20"
-        fill={this.props.fill}
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <circle
-          cx="8.79099"
-          cy="8.06162"
-          r="7.31101"
-          stroke={this.props.stroke}
-          strokeWidth="1.5"
-        />
-        <path
-          d="M14.4546 13.0656L19.939 18.5494"
-          stroke={this.props.stroke}
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
-    );
-  }
-}
+const SearchIcon = (props: SearchIconProps) => {
+  return (
+    <svg
+      className={props.className}
+      width="21"
+      height="20"
+      viewBox="0 0 21 20"
+      fill={props.fill}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="8.79099" cy="8.06162" r="7.31101" stroke={props.stroke} strokeWidth="1.5" />
+      <path
+        d="M14.4546 13.0656L19.939 18.5494"
+        stroke={props.stroke}
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
 
 export default SearchIcon;
